d4a888de40f195a5c0859a20a5571adc2e8cad8e
HH <hans.herzl@live.com>
2018-07-20 12:04:14 -0600

    1.0.0-beta-sun-build08 Version: Core package update.
    Quick start: https://www.codeproject.com/Tips/1229909/Scaffolding-ASP-NET-Core-with-CatFactory

--- CatFactory.AspNetCore/CatFactory.AspNetCore.Tests/ImportTests.cs
+++ CatFactory.AspNetCore/CatFactory.AspNetCore.Tests/ImportTests.cs
@@ -77,5 +77,38 @@ namespace CatFactory.AspNetCore.Tests
 
             aspNetCoreProject.ScaffoldAspNetCore();
         }
+
+        [Fact]
+        public void TestControllerScaffoldingFromAdventureWorks2017Database()
+        {
+            // Import database
+            var database = SqlServerDatabaseFactory
+                .Import(SqlServerDatabaseFactory.GetLogger(), "server=(local);database=AdventureWorks2017;integrated security=yes;", "dbo.sysdiagrams");
+
+            // Create instance of Entity Framework Core Project
+            var entityFrameworkProject = new EntityFrameworkCoreProject
+            {
+                Name = "Northwind",
+                Database = database,
+                OutputDirectory = "C:\\Temp\\CatFactory.AspNetCore\\AdventureWorks2017.Core"
+            };
+
+            // Apply settings for project
+            entityFrameworkProject.GlobalSelection(settings => settings.ForceOverwrite = true);
+            entityFrameworkProject.Select("Sales.Order", settings => settings.EntitiesWithDataContracts = true);
+
+            // Build features for project, group all entities by schema into a feature
+            entityFrameworkProject.BuildFeatures();
+
+            // Scaffolding =^^=
+            entityFrameworkProject
+                .ScaffoldEntityLayer()
+                .ScaffoldDataLayer();
+
+            var aspNetCoreProject = entityFrameworkProject
+                .CreateAspNetCoreProject("Northwind.AspNetCore", "C:\\Temp\\CatFactory.AspNetCore\\AdventureWorks2017.Api", entityFrameworkProject.Database);
+
+            aspNetCoreProject.ScaffoldAspNetCore();
+        }
     }
 }
